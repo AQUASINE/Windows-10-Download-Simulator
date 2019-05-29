aeb2b02d375a17bd102d705642f91aeb687ed8c0
2102062 <2102062@geneva304.org>
2019-05-29 08:30:53 -0500

    Movement works

--- project/main/Main.py
+++ project/main/Main.py
@@ -1,17 +1,20 @@
 import tkinter as tk
 import math
+from functools import partial
 
-
+root = tk.Tk()
 class Main(tk.Frame):
     canvas_width = 1920
     canvas_height = 1000
     score_increase = 1
+    x, y = (0, 90)
+
 
     def __init__(self, *args, **kwargs):
         tk.Frame.__init__(self, *args, **kwargs)
         self.curve_radius = 125
         self.angle_start = 90
-        self.score = 0
+        self.score = 20
         self.angle_length = self.score/100.0*360.0
 
         self.canvas = tk.Canvas(self, width=self.canvas_width, height=self.canvas_height)
@@ -34,7 +37,7 @@ class Main(tk.Frame):
                                                      text="Copying Files",
                                                      fill="#3479D7", justify="center", font=("Microsoft YaHei UI", 11),
                                                      anchor="center")
-        self.installing_features_id = self.canvas.create_text((self.canvas_width / 2), (self.canvas_height - 40),
+        self.installing_features_id = self.canvas.create_text((self.canvas_width / 2 - 8), (self.canvas_height - 40),
                                                               text="Installing features and drivers",
                                                               fill="#999999", justify="center",
                                                               font=("Microsoft YaHei UI", 11), anchor="center")
@@ -64,6 +67,7 @@ class Main(tk.Frame):
                                              outline="#3479D7", style="arc", width=4.6, extent=-self.angle_length,
                                              start=self.angle_start)
         self.increase_score()
+        root.bind('<Motion>', partial(self.motion))
     def update_bar(self):
         self.angle_length = self.score/100.0*360.0
 
@@ -73,13 +77,22 @@ class Main(tk.Frame):
         self.canvas.itemconfig(self.arc_id, extent=-self.angle_length)
         self.canvas.itemconfig(self.percent_id, text=str(int(self.angle_length / 360 * 100)) + "%")
 
-    def update_rotation(self, event):
-        self.angle_start += 1
+    def update_rotation(self, x, y):
+        self.angle_start = math.degrees(math.atan2((x-self.canvas_width/2),(y-self.canvas_height/2)))-15-self.angle_length/2
         self.canvas.itemconfig(self.arc_id, start=self.angle_start)
+        print(self.angle_start)
+
+    def motion(self, event):
+        global x, y
+        x, y = event.x, event.y
+        self.update_rotation(x, y)
+
+
 
 
 if __name__ == "__main__":
-    root = tk.Tk()
+
     view = Main(root)
     view.pack(side="top", fill="both", expand=True)
+
     root.mainloop()
